7eb18c7420780032957ce9f65609bf9c74d1adf6
Arti Nalegaonkar <analegaonkar@gmail.com>
2023-08-01 14:16:22 +0000

    added ref and src

--- models/staging/stg_dept_tbl.sql
+++ models/staging/stg_dept_tbl.sql
@@ -3,4 +3,4 @@ select   DEPARTMENT_ID  dept_id
     , EFF_STATUS            effective_staus
     , DESCR50               dept_descr
     , EXTRACT_DATETIME      extracted_datetime
-  from MANAGE_DB.EXTERNAL_STAGE.RAW_DEPT_TBL
+  from {{ source('dept','RAW_DEPT_TBL')}}
