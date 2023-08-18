eefebaf4fada239eaf08f35abcc018511161f389
Arti Nalegaonkar <artitechnical@gmail.com>
2023-08-18 17:17:03 +0000

    Changed dept description

--- models/core/mart/HR/dept_head_vw.sql
+++ models/core/mart/HR/dept_head_vw.sql
@@ -2,7 +2,7 @@ with
 final as
 (select 
       D.DEPT_ID				department_id
-	 , D.DEPT_DESCR				department
+	 , D.DEPT_DESCR				department_descr
 	 , D.EFFECTIVE_DATE			effective_date
 	 , D.EFFECTIVE_STAUS		effective_status
 	 , H.DEPT_HEAD_ID           department_function_head_id
